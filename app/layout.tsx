3a9e45e364419acb7345cfacc2fad82c787f1ee3
Ryan <ralamo33@gmail.com>
2023-01-10 21:50:58 -0500

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement, ReactNode } from "react";
 import '../styles/globals.css'
+import { AnalyticsWrapper } from "./components/analytics";
 
 export default function RootLayout({children}: {children: ReactNode}): ReactElement {
     return <html>
         <body>
             <div>
               {children}
+              <AnalyticsWrapper />
             </div>
         </body>
     </html>
