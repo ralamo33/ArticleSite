433c03dd00c476c0df0c4a1ee70e91cdd3230890
Ryan <ralamo33@gmail.com>
2023-01-13 19:14:11 -0500

    Google analytics complete

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,19 @@
+import Script from "next/script";
+
 export default function Head() {
+    const googleAnalyticsTag = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-KVEKCRFHM9');
+    `;
+
     return (
         <>
             <title>Gaming Knowledge</title>
             <meta name="description">Entertaining articles on gaming trends and controversy</meta>
+            <Script id="google-analytics-home" async  src="https://www.googletagmanager.com/gtag/js?id=G-KVEKCRFHM9" />
+            <Script dangerouslySetInnerHTML={{__html: googleAnalyticsTag}} />
         </>
     );
 }
