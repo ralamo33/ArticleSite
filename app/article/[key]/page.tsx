e6c0d1368ffc1003233b0f785223d19f0e57be7d
Ryan <ralamo33@gmail.com>
2023-01-21 12:58:20 -0500

    Articles look decentish now

--- app/article/[key]/page.tsx
+++ app/article/[key]/page.tsx
@@ -14,7 +14,7 @@ export default async function Article({params}: Params): Promise<ReactElement> {
     const key = params.key;
     const data = await getArticleData(key);
 
-    return <div className={`${standardFont.className} mr-20 ml-20 mt-7`} >
+    return <div className={`${standardFont.className} mr-20 ml-20 mt-7 border-4 border-solid rounded-lg px-10`} >
         <h1 className={`text-center ${majorFont.className} text-4xl`}>{data.title}</h1>
         {/* <p className="text-center italic text-gray-700">{data.description}</p> */}
         {data.body.split("\n").map((paragraph) => <p className={`text-left text-black mb-8 text-lg`} key={`Paragraph${paragraph.length}${paragraph.split(" ")[0]}`}>{paragraph}</p>)}
